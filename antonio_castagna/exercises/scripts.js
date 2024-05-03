55f81c6e33d5bd771cfc5eeb4f17b8931c295aed
Antonio Castagna <antocasta04@gmail.com>
2024-05-03 13:39:59 +0200

    Antonio Castagna: Creazione bottone in Html con file js e css.

---
+++ antonio_castagna/exercises/scripts.js
@@ -0,0 +1,24 @@
+var diminuisciNumero = document.getElementById("diminuisci");
+var aumentaNumero = document.getElementById("aumenta");
+
+diminuisciNumero.addEventListener("click", function () {
+  diminuisci();
+});
+
+aumentaNumero.addEventListener("click", function () {
+  aumenta();
+});
+
+function diminuisci() {
+  var numero = document.getElementById("numero");
+  var counter = parseInt(numero.textContent);
+  counter--;
+  numero.textContent = counter;
+}
+
+function aumenta() {
+  var numero = document.getElementById("numero");
+  var counter = parseInt(numero.textContent);
+  counter++;
+  numero.textContent = counter;
+}
