22e16c3e6af3c65aaacac4f862c5200c753817df
christian <chretianuccio16@gmail.com>
2024-05-08 13:51:50 +0200

    christian_suriano: implementazione html javascript e sheet, svolgimento di alcuni esercizi

--- christian_suriano/exercises/ex2/script.js
+++ christian_suriano/exercises/ex2/script.js
@@ -1,8 +1,16 @@
-//concatenazione array
+//----------------------esercizi----------------------//
+//1.Dato un array di numeri, triplica ciascun numero presente nell'array.
+const numbers = [1, 2, 3, 4, 5];
+function onMap(el, index, array) {
+  return el * 3;
+}
+const newArray = numbers.map(onMap);
 
-const array1 = [1, 2, 3];
-const array2 = [4, 5, 6];
+// 2.Dato un array di numeri, filtra solo i numeri dispari maggiori di 3.
 
-const arrayConcatenato = [...array1, ...array2];
-
-console.log(arrayConcatenato);
+const numbers = [1, 2, 3, 4, 5, 6, 7, 23];
+function onFilter(el, index, array) {
+  if (el % 2 !== 0 && el > 3) return true;
+  else return false;
+}
+const newArray = numbers.filter(onFilter);
