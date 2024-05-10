f0ef633612efeadb9f9a13c3bd1aec47313c7769
Riccardo Genova <r.genova@bitrocket.dev>
2024-05-10 12:58:44 +0200

    chore(maintainer): update eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@ import pluginJs from '@eslint/js';
 
 export default [
   {
+    files: ["src/**/*.js"],
     languageOptions: {
       globals: globals.browser,
     },
@@ -12,7 +13,7 @@ export default [
       'no-unused-vars': 'warn',
       'no-undef': 'warn',
     },
-    ignores: ['node_modules'],
+    ignores: ['node_modules',"**/*.config.js", "!**/eslint.config.js"],
   },
   pluginJs.configs.recommended,
 ];
